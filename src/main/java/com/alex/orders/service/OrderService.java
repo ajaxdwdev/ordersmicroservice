7735f854a3a8d847a1c093d10a1e4367b447e9be
Ajaxdw <100229709+ajaxdwdev@users.noreply.github.com>
2023-07-25 14:41:34 -0400

    notes on where to call external product validation

--- src/main/java/com/alex/orders/service/OrderService.java
+++ src/main/java/com/alex/orders/service/OrderService.java
@@ -1,48 +1,47 @@
 package com.alex.orders.service;
 
+import com.alex.orders.entity.Order;
+import com.alex.orders.repository.OrderRepo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.alex.orders.entity.Order;
-import com.alex.orders.repository.OrderRepo;
 
 @Service
 public class OrderService {
 
-    @Autowired
-    OrderRepo orderRepo;
+  @Autowired
+  OrderRepo orderRepo;
 
-    public List<Order> getAllOrder() {
-        List<Order> ordersList = orderRepo.findAll();
-        
-        if(ordersList.size() > 0 ){
-            return ordersList;
-        }else{
-            return new ArrayList<Order>();
-        }
+  public List<Order> getAllOrder() {
+    List<Order> ordersList = orderRepo.findAll();
+
+    if (ordersList.size() > 0) {
+      return ordersList;
+    } else {
+      return new ArrayList<Order>();
     }
+  }
 
-    public Optional<Order> getOrderById(Long id) {
-        return orderRepo.findById(id);
-    }
+  public Optional<Order> getOrderById(Long id) {
+    return orderRepo.findById(id);
+  }
 
-    public Order createOrder(Order order) {
-        Order newOrder = new Order();
-        newOrder.setProductDescription(order.getProductDescription());
-        newOrder.setOrderPrice(order.getOrderPrice());
-        newOrder.setProducts(order.getProducts());
-        newOrder = orderRepo.save(newOrder);
+  //maybe invoke the external validation here when creating an order
+  //check in repo if there already exists an order with the same list of products
+  //swagger by openapi?
+  public Order createOrder(Order order) {
+    Order newOrder = new Order();
+    newOrder.setProductDescription(order.getProductDescription());
+    newOrder.setOrderPrice(order.getOrderPrice());
+    newOrder.setProducts(order.getProducts()); //compare this list with other orders in repo
+    newOrder = orderRepo.save(newOrder);
 
-        return newOrder;
-    }
+    return newOrder;
+  }
 
-    public void deleteOrder(Long id) {
-        orderRepo.deleteById(id);
-    }
-
-
+  public void deleteOrder(Long id) {
+    orderRepo.deleteById(id);
+  }
 }
