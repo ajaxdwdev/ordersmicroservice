94e22da2deb6a5ecde69bbc253b1ef49b0d18979
Ajaxdw <100229709+ajaxdwdev@users.noreply.github.com>
2023-07-28 10:39:39 -0400

    removed unused imports/old code from 1st attempt

--- src/main/java/com/alex/orders/controller/OrderController.java
+++ src/main/java/com/alex/orders/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.alex.orders.entity.Order;
 import com.alex.orders.exceptions.OrderProductAlreadyExistsException;
 import com.alex.orders.service.OrderService;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/alex/orders/service/OrderService.java
+++ src/main/java/com/alex/orders/service/OrderService.java
@@ -5,9 +5,7 @@ import com.alex.orders.entity.OrderProducts;
 import com.alex.orders.exceptions.OrderProductAlreadyExistsException;
 import com.alex.orders.repository.OrderProductsRepo;
 import com.alex.orders.repository.OrderRepo;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -53,38 +51,4 @@ public class OrderService {
     }
     return false;
   }
-  /*
-  public List<Order> getAllOrder() {
-    List<Order> ordersList = orderRepo.findAll();
-
-    if (ordersList.size() > 0) {
-      return ordersList;
-    } else {
-      return new ArrayList<Order>();
-    }
-  }
-
-  public Optional<Order> getOrderById(Long id) {
-    return orderRepo.findById(id);
-  }
-
-  public Order createOrder(Order order) {
-    Order newOrder = new Order();
-    newOrder.setOrderDescription(order.getOrderDescription());
-    newOrder.setOrderPrice(order.getOrderPrice());
-    newOrder.setOrderProducts(order.getOrderProducts()); //compare this list with other orders in repo
-    newOrder = orderRepo.save(newOrder);
-
-    return newOrder;
-  }
-
-  public void deleteOrder(Long id) {
-    orderRepo.deleteById(id);
-  }
-
-  public boolean isOrderWithProductsExists(List<OrderProducts> orderProducts) {
-    return orderRepo.existsOrderWithProducts(orderProducts);
-  }
-  */
-
 }
